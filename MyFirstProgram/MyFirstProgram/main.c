8d6744baac0b4d6104236e32c202f116212d5433
Quinn Supplee <quinn@Quinn-Supplees-iMac.local>
2010-01-26 19:07:34 -0800

    created simple calculator

--- MyFirstProgram/MyFirstProgram/main.c
+++ MyFirstProgram/MyFirstProgram/main.c
@@ -1,11 +1,44 @@
 #include	<stdio.h> // Defines printf etc.
+#include	<stdbool.h>
 
-int	main() 
+int main ()
 {
-	int userInput;
-	printf("Please enter your age: ");
-	scanf("%d", &userInput);
-	fpurge(stdin);
-	printf("You are %d years old.\n", userInput);
+
+	int		vFirstArg,
+			vSecondArg;
+	char	vOperation;
+	bool	vFinished;
+	
+	// make sure ouf flag is initialized
+	vFinished = false;
+	
+	// now loop
+	
+	while (vFinished != true) {
+		printf("What operation do you want to do?");
+		scanf("%c", &vOperation);
+		fpurge(stdin);
+		
+		if (vOperation == '+') {
+			printf("Enter left argument: ");
+			scanf("%d", &vFirstArg);
+			fpurge(stdin);
+			
+			printf("Enter right argument: ");
+			scanf("%d", &vSecondArg);
+			
+			fpurge(stdin);
+			
+			printf("\n%d + %d = %d\n", vFirstArg, vSecondArg, vFirstArg + vSecondArg);
+		} else {
+			vFinished = true;
+		}
+
+		
+			printf("Finished.\n");
+	}
+	
 	return 0;
+	
+
 }
